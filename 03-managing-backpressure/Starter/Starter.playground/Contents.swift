1aad1f7c874a104e8b218a1ad93a95f51b5a1e0f
🤨 <jorge_mz@icloud.com>
2021-04-22 20:38:39 -0500

    - manage demand

--- 03-managing-backpressure/Starter/Starter.playground/Contents.swift
+++ 03-managing-backpressure/Starter/Starter.playground/Contents.swift
@@ -6,7 +6,46 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "Dynamically adjusting demand") {
+  final class IntSubscriber: Subscriber {
+    typealias Input = Int
+    typealias Failure = Never
+    
+    func receive(subscription: Subscription) {
+      subscription.request(.max(2))
+    }
+    
+    func receive(_ input: Int) -> Subscribers.Demand {
+      print("Received value", input)
+      
+      switch input {
+      case 1:
+        return .max(2)
+      case 2:
+        return .max(1)
+      default:
+        return .none
+      }
+    }
+    
+    func receive(completion: Subscribers.Completion<Never>) {
+      print("Received completion", completion)
+    }
+  }
+  
+  let subscriber = IntSubscriber()
+  
+  let subject = PassthroughSubject<Int, Never>()
+  
+  subject.subscribe(subscriber)
+  
+  subject.send(1)
+  subject.send(2)
+  subject.send(3)
+  subject.send(4)
+  subject.send(5)
+  subject.send(6)
+}
 
 /// Copyright (c) 2021 Razeware LLC
 ///
